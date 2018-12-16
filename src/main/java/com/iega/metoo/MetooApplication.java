493daec064e044cc7370eca387490ccbb2b9562b
Thomaz Bittencourt <thomaz_bittencourt@icloud.com>
2018-12-16 02:33:17 -0300

    Criação do projeto

---
+++ src/main/java/com/iega/metoo/MetooApplication.java
@@ -0,0 +1,14 @@
+package com.iega.metoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetooApplication.class, args);
+	}
+
+}
+
